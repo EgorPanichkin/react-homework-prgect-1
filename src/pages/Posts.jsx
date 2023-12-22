b84320488f00265e5259af5c83a78ab4362b2220
EgorLogan73 <gogaspb98@gmail.com>
2023-12-22 20:17:33 +0600

    add pages navigation with useSearchParams

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,30 +1,46 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import style from '../styles/Posts.module.css'
+
 
 export default function Posts() {
   const [postData, setPostData] = useState()
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 })
+  const [pageArray, setPageArray] = useState([])
 
-  async function fetchPosts() {
+  async function fetchPosts(searchParams) {
     try {
-      const responce = await axios.get('https://dummyjson.com/posts')
-      console.log(responce);
-      setPostData(responce.data)
+      const responce = await axios.get(`https://dummyjson.com/posts?limit=10&skip=${(searchParams.get('page') - 1) * 10}`)
+      setPostData(responce.data);
+
+      const pages = [] // Считаем количество страниц и создаем массив страниц
+      for (let i = 1; i <= (Math.ceil(responce.data.total / 10)); i++) {
+        pages.push(i)
+      }
+      setPageArray(pages)
     } catch (error) {
       console.error(`Responce ERROR`, error.message);
     }
   }
-  
+
   useEffect(() => {
-    fetchPosts()
-  }, [])
+    fetchPosts(searchParams)
+  }, [searchParams])
+
+  function handleClick(target) {
+    setSearchParams({ page: target.value })
+  }
 
   return (
     <div className="wrapper">
-      {postData && (
-        <div>
-          {postData.posts.map(post => {
+        <div> 
+          <div className={style.buttonBox}>
+          {pageArray.map(item => 
+            <button onClick={(e) => handleClick(e.target)} key={item} value={item}>{item}</button>
+          )}
+          </div>
+          {postData?.posts.map(post => {
             return (
               <div key={post.id} className={style.post}>
                 <Link to={`/Posts/${post.id}`}><h3 className={style.header}>{post.title}</h3></Link>
@@ -40,8 +56,6 @@ export default function Posts() {
             )
           })}
         </div>
-      )}
     </div>
   )
 }
-
